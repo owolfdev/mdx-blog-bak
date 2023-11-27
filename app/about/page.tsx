0ec195b67fddeece838a88d62502dcf82f1095b9
owolfdev <owolfdev@gmail.com>
2023-11-28 06:26:47 +0700

    updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
-function About() {
-  return <div>About</div>;
+export default function About() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        about
+      </div>
+    </main>
+  );
 }
-
-export default About;

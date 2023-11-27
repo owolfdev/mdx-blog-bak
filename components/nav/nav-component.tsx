5844c75312ab39bc19784f73a722a7c46b18c3a5
owolfdev <owolfdev@gmail.com>
2023-11-28 06:40:59 +0700

    updates

--- components/nav/nav-component.tsx
+++ components/nav/nav-component.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { Button, buttonVariants } from "@/components/ui/button";
 
 const NavComponent = () => {

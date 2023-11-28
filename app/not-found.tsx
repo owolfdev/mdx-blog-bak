996ef289146c0d5af773b293f0ab722a3215fbbd
owolfdev <owolfdev@gmail.com>
2023-11-29 02:55:22 +0700

    update 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h2>Not Found</h2>
         <p>Could not find requested resource</p>
-        <Link href="/">Return Home</Link>
+        <a href="/">Return Home</a>
       </div>
     </main>
   );

c49538ff4cb3a2c568a55a4261a4a9442606817b
owolfdev <owolfdev@gmail.com>
2023-12-01 16:48:45 +0700

    update

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,36 +4,37 @@ import matter from "gray-matter";
 import Link from "next/link";
 
 export default function Home() {
-  const blogDirectory = path.join(process.cwd(), "../mdx-blog/data/posts");
-  const fileNames = fs.readdirSync(blogDirectory);
+  const blogDirectory = path.join(process.cwd(), "../data/posts");
+  // const fileNames = fs.readdirSync(blogDirectory);
 
-  const blogs = fileNames.map((fileName) => {
-    const slug = fileName.replace(".mdx", "");
-    const fullPath = path.join(blogDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+  // const blogs = fileNames.map((fileName) => {
+  //   const slug = fileName.replace(".mdx", "");
+  //   const fullPath = path.join(blogDirectory, fileName);
+  //   const fileContents = fs.readFileSync(fullPath, "utf8");
 
-    const { data: frontMatter } = matter(fileContents);
+  //   const { data: frontMatter } = matter(fileContents);
 
-    const date = new Date(frontMatter.date);
+  //   const date = new Date(frontMatter.date);
 
-    // Format the date to a readable string format
-    // For example, "October 1, 2021"
-    const formattedDate = date.toLocaleDateString("en-US", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
+  //   // Format the date to a readable string format
+  //   // For example, "October 1, 2021"
+  //   const formattedDate = date.toLocaleDateString("en-US", {
+  //     year: "numeric",
+  //     month: "long",
+  //     day: "numeric",
+  //   });
 
-    return {
-      slug,
-      formattedDate,
-      meta: frontMatter,
-    };
-  });
+  //   return {
+  //     slug,
+  //     formattedDate,
+  //     meta: frontMatter,
+  //   };
+  // });
 
   return (
     <div className="">
-      <div className="flex flex-col gap-8">
+      <div>{JSON.stringify(blogDirectory)}</div>
+      {/* <div className="flex flex-col gap-8">
         <h1 className="text-3xl sm:text-5xl font-bold text-center">
           Next.js MDX Blog
         </h1>
@@ -51,7 +52,7 @@ export default function Home() {
             ))}
           </ul>
         </section>
-      </div>
+      </div> */}
     </div>
   );
 }
